9628faf4496dbde06b8691e8b4846fcd5f670808
OliverGo <olivergomesdeoliveira03@gmail.com>
2025-06-03 20:12:16 -0300

    removendo arquivo e modificando código

--- códigos/analise.py
+++
@@ -1,7 +0,0 @@
-import numpy as np 
-import pandas as pd
-n =  [12, 45, 67, 23, 89, 34, 22, 90, 56, 78]
-cal = np.average(n)
-abaixo_media  = [num for num in n if num > cal]
-df = pd.DataFrame(abaixo_media)
-df.to_csv('numeros_maiores_que_media.csv')
