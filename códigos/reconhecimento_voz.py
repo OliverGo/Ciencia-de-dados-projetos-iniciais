5bb01b3a313954e29d5920914e181d8d10193088
OliverGo <olivergomesdeoliveira03@gmail.com>
2025-06-27 18:02:23 -0300

    adicionando tradutor para projeto furuto

--- códigos/reconhecimento_voz.py
+++ códigos/reconhecimento_voz.py
@@ -1,12 +1,33 @@
 import speech_recognition as sr
 import tkinter as tk 
 import chess
-
+import re
 
 
      
 
 recognizer = sr.Recognizer()
+
+def normalizar_numeros(texto):
+    numeros = {
+        "um": "1", "dois": "2", "três": "3", "tres": "3",
+        "quatro": "4", "cinco": "5", "seis": "6",
+        "sete": "7", "oito": "8"
+    }
+    for palavra, numero in numeros.items():
+        texto = texto.replace(palavra, numero)
+    return texto
+
+
+def converte_comando(texto):
+    
+    texto = texto.lower()
+    texto = normalizar_numeros(texto)
+    texto = texto.replace(" ", "").replace("para", "")
+    match = re.match(r"([a-h][1-8])([a-h][1-8])", texto)
+    if match:
+         return match.group(1) + match.group(2)
+    return None
 
 while True:
     with sr.Microphone() as mic:
