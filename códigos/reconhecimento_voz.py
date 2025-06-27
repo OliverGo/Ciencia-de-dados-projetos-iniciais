c6f8e9501bfdabc9346fde43091bdc05ec7a63a7
OliverGo <olivergomesdeoliveira03@gmail.com>
2025-06-27 13:07:14 -0300

    Adicionando tratamento de erros

--- códigos/reconhecimento_voz.py
+++ códigos/reconhecimento_voz.py
@@ -1,11 +1,23 @@
 import speech_recognition as sr
+import tkinter as tk 
+import chess
+
+
+
+     
 
 recognizer = sr.Recognizer()
 
 while True:
     with sr.Microphone() as mic:
-        recognizer.adjust_for_ambient_noise(mic, duration=0.5)
+        recognizer.adjust_for_ambient_noise(mic, duration=0.3)
         audio = recognizer.listen(mic)
-        texto = recognizer.recognize_google(audio, language='pt-BR')
-        texto = texto.lower()
-        print(f"{texto}")
+
+        try:
+            texto = recognizer.recognize_google(audio, language='pt-BR')
+            texto = texto.lower()
+            print(f"{texto}")
+        except sr.UnknownValueError as erro:
+            print("Não entendi o que você disse")
+            recognizer
+            continue 
